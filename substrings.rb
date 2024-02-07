c9b645eba3239eea0decad623d897fe6c2e936b2
Carlos E <58950500+CarlosElizalde98@users.noreply.github.com>
2024-02-07 11:21:43 -0600

    Added logic for substring selection

--- substrings.rb
+++ substrings.rb
@@ -1,2 +1,22 @@
-def substrings(words = {}, dictionary) 
+def substrings(words, dictionary)
+  newWords = words.downcase.split(" ")
+  strings = Hash.new
+  newWords.each do |word|
+    dictionary.each do |value|
+      if word.include?(value)
+        strings[value] = 0
+      end
+    end
+  end
+  dictionary.each do |word|
+    newWords.each_with_index do |value, index|
+    if newWords[index].include?(word)
+      strings[word] += 1
+    end
+  end
 end
+  return p strings
+end
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
